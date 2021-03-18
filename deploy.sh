fb98e7bed81dbb89318455114f3e77b34398e52d
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-03-18 05:13:19 +0100

    cd: deploy.sh that detaches a volume if cfn replaces the instance

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 stack_exists() { # $stack_name
-    &>/dev/null aws cloudformation describe-stacks --stack-name $1
+  &>/dev/null aws cloudformation describe-stacks --stack-name $1
 }
 
 stack_name=zyzx
@@ -13,22 +13,49 @@ else
   change_set_type=CREATE
 fi
 
+echo "creatin change set"
+
 aws cloudformation create-change-set \
   --stack-name $stack_name \
   --change-set-name $change_set_name \
   --change-set-type $change_set_type \
   --template-body file://stack.yml
 
-aws cloudformation describe-change-set \
-  --stack-name $stack_name \
-  --change-set-name $change_set_name \
-| \
-jq '.Changes'
+if [[ $change_set_type -eq UPDATE ]]; then
+  change_set="$( \
+    aws cloudformation describe-change-set \
+      --stack-name $stack_name \
+      --change-set-name $change_set_name \
+  )"
 
-# example change sets
-# https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-updating-stacks-changesets-samples.html
-# TODO
-    # + inspect changes & check whether the ec2 needs to be replaced
-    # + if yes detach the volume beforehand
-    # + `aws cloudformation execute-change-set ...`
-    
+  stack="$( \
+    aws cloudformation describe-stacks \
+      --stack-name $stack_name \
+  )"
+
+  instance="$( \
+    jq -r '.Changes[] | select(.ResourceChange.LogicalResourceId == "Instance")' <<< "$change_set" \
+  )"
+
+  volume_id="$( \
+    jq -r '.Stacks[] | select(.StackName == "zyzx") | .Outputs[] | select(.OutputKey == "VolumeId") | .OutputValue' <<< "$stack" \
+  )"
+
+  instance_replacement="$(jq -r '.ResourceChange.Replacement' <<< "$instance")"
+  instance_id="$(jq -r '.ResourceChange.PhysicalResourceId' <<< "$instance")"
+
+  if [ "$instance_replacement" == "True" ]; then
+    echo "detachin volume"
+
+    aws ec2 detach-volume \
+      --device /dev/sdh \
+      --instance-id $instance_id \
+      --volume-id $volume_id
+  fi
+fi
+
+echo "executin change set"
+
+aws cloudformation execute-change-set \
+  --stack-name $stack_name \
+  --change-set-name $change_set_name
